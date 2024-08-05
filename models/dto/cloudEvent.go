c1541897b79982dd0af11852cb46cf4192b8bf7e
毛毛 <5427081+maomao19970922@user.noreply.gitee.com>
2024-08-05 11:01:11 +0800

    debug

--- models/dto/cloudEvent.go
+++ models/dto/cloudEvent.go
@@ -2,7 +2,6 @@ package dto
 
 import (
 	"encoding/json"
-	"fmt"
 
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/opensourceways/message-transfer/common/postgresql"
@@ -23,22 +22,19 @@ func NewCloudEvents() CloudEvents {
 
 func (event CloudEvents) Message() ([]byte, error) {
 	body, err := json.Marshal(event)
-	logrus.Info(body)
 	return body, err
 }
 
 func (event CloudEvents) SaveDb() {
 	eventDO := event.toCloudEventDO()
-	res := postgresql.DB().Clauses(clause.OnConflict{
+	postgresql.DB().Clauses(clause.OnConflict{
 		Columns:   []clause.Column{{Name: "event_id"}, {Name: "source"}},
 		DoUpdates: clause.AssignmentColumns([]string{"event_id", "source_url", "source_group", "summary", "data_schema", "data_content_type", "spec_version", "time", "user", "data_json", "title", "related_users"}),
 	}).Create(&eventDO)
-	logrus.Info(res)
 	logrus.Info("插入成功")
 }
 
 func (event CloudEvents) toCloudEventDO() do.MessageCloudEventDO {
-	fmt.Println(event)
 	//relatedUsers := strings.Split(event.Extensions()["relatedusers"].(string), ",")
 	messageCloudEventDO := do.MessageCloudEventDO{
 		Source:          event.Source(),
