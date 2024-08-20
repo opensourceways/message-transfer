c220cd96f896cb9c709270bf774545a8f3daad8e
毛毛 <5427081+maomao19970922@user.noreply.gitee.com>
2024-08-20 14:19:10 +0800

    gitee消息增加siggroupname字段

--- models/dto/gitee.go
+++ models/dto/gitee.go
@@ -6,16 +6,20 @@ import (
 
 type GiteeIssueRaw struct {
 	gitee.IssueEvent
+	SigGroupName string `json:"sig_group_name"`
 }
 
 type GiteePushRaw struct {
 	gitee.PushEvent
+	SigGroupName string `json:"sig_group_name"`
 }
 
 type GiteePrRaw struct {
 	gitee.PullRequestEvent
+	SigGroupName string `json:"sig_group_name"`
 }
 
 type GiteeNoteRaw struct {
 	gitee.NoteEvent
+	SigGroupName string `json:"sig_group_name"`
 }
