32e90d67491799cbf94cb950850949b2b97bf843
Hourunze1997 <1043170898@qq.com>
2024-12-10 10:49:39 +0800

    test

--- service/handleEvent.go
+++ service/handleEvent.go
@@ -14,6 +14,7 @@ import (
 func handleEvent(rawEvent dto.RawEvent, cfg kafka.ConsumeConfig) error {
 	event := rawEvent.ToCloudEventsByConfig()
 	if event.ID() == "" {
+		logrus.Errorf("event id is empty")
 		return nil
 	}
 	rawEvent.GetTodoUsers(event)
